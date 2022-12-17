95c6e1074bd38a4b868c0ad497d4f63e952117d2
이윤기 <ahacross@naver.com>
2022-12-17 21:37:23 +0900

    수정

--- src/plugins/vfm/modalExample.vue
+++ src/plugins/vfm/modalExample.vue
@@ -6,8 +6,9 @@
 
 <script>
 import modalMixin from '@/plugins/vfm/modalMixin'
+// 팝업페이지를 만들 땐 위에 mixin 파일을 설정!!
 // 팝업페이지는 받아야 하는 것은 props에 정의하고
-// 리턴해야 하는 data는 data에 returnValue에 값을 넣어야 한다.
+// 리턴해야 하는 data는 setReturnValue 넣어주면 됩니다.
 export default {
   name: 'modalExample',
   mixins: [modalMixin],
@@ -25,6 +26,7 @@ export default {
     }
   },
   mounted() {
+    // 이런 식으로 returnValue만 넣어주면 끝.
     this.setReturnValue('test123123')
   }
 }
