07c355455533e2f529ea63d7cc08ef57c00971a4
이윤기 <ahacross@naver.com>
2022-12-17 21:31:46 +0900

    수정

--- src/plugins/vfm/index.js
+++ src/plugins/vfm/index.js
@@ -1,9 +1,10 @@
 import { getParams, getParamsModal } from '@/plugins/vfm/hoc/vfmParams'
-import { $vfm } from 'vue-final-modal'
+// import { $vfm } from 'vue-final-modal'
 import './transition.css'
 
 export default {
   install(app) {
+    const $vfm = app.config.globalProperties.$vfm
     app.config.globalProperties.$alert = (params, bind) => {
       return new Promise(resolve => {
         $vfm.show(getParams({ resolve, params, bind }))
