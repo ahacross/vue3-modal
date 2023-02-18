0e51c427835fd8a170c8e6d8f4a6171b5847be01
이윤기 <ahacross@naver.com>
2023-02-18 21:35:41 +0900

    cache meta tag 추가

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// import { createHtmlPlugin } from 'vite-plugin-html'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
